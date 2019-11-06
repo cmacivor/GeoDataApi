558c0b4fb6c457bd75223c6fee95278f89455fba
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2019-11-06 09:23:15 -0500

    add addresscandidates controller

---
+++ GeoDataService/Controllers/AddressCandidatesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GeoDataService.Controllers
+{
+  
+    public class AddressCandidatesController : ApiController
+    {
+        //make this static, according to this article: https://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/
+        private static HttpClient _httpClient;
+
+        public AddressCandidatesController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://gisdev.richmondgov.com/arcgis/rest/services/Geocode/RichmondAddress/GeocodeServer/findAddressCandidates");
+        }
+
+
+        public async Task<IHttpActionResult> Get(string street)
+        {
+            var arguments = new Dictionary<string, string>();
+            arguments.Add("Street", street);
+            arguments.Add("f", "pjson"); //tells the GIS service to return JSON instead of HTML
+
+            var request = new HttpRequestMessage(HttpMethod.Post, _httpClient.BaseAddress)
+            {
+                Content = new FormUrlEncodedContent(arguments)
+            };
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+            }
+
+            return null;
+        }
+    }
+}
