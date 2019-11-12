9d65edb6382997c073271dc28f226c0d7e5ebbe9
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2019-11-12 17:07:16 -0500

    do some work to get cordeploy working

--- GeoDataService/App_Start/WebApiConfig.cs
+++ GeoDataService/App_Start/WebApiConfig.cs
@@ -14,6 +14,8 @@ namespace GeoDataService
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
