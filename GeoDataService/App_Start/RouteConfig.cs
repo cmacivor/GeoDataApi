af30e646dbbfedc9e952533f5f28daa2bf8975fd
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2019-11-13 10:07:39 -0500

    remove this cruft

--- GeoDataService/App_Start/RouteConfig.cs
+++ GeoDataService/App_Start/RouteConfig.cs
@@ -14,14 +14,7 @@ namespace GeoDataService
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            //routes.MapHttpRoute(
-            //name: "swagger_root",
-            //routeTemplate: "",
-            //defaults: null,
-            //constraints: null,
-            //handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger");
-
+            
             routes.MapHttpRoute(name: "swagger_root",
                 routeTemplate: "",
                 defaults: null,
--- GeoDataService/Global.asax.cs
+++ GeoDataService/Global.asax.cs
@@ -13,13 +13,10 @@ namespace GeoDataService
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            //AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            //BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            //SwaggerConfig.Register();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);      
         }
     }
 }
