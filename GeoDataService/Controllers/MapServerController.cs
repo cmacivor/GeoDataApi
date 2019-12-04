8a1133157feb8c4c60f9583037a697a6c47dfe75
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2019-12-04 15:56:09 -0500

    cleanup

--- GeoDataService/Controllers/MapServerController.cs
+++ GeoDataService/Controllers/MapServerController.cs
@@ -30,12 +30,6 @@ namespace GeoDataService.Controllers
 
             mapServerClient.CommonBoundariesApiUrlParameters = ConfigurationManager.AppSettings["CommonBoundariesApiParameters"];
 
-            //fallback policy
-            //var policy = Policy<Task<MapServerReturnResult>>
-            //    .Handle<Exception>()                
-            //    .Fallback<Task<MapServerReturnResult>>(() => GetFromDatabase(streetAddress));
-
-            //Handle both exceptions and return values in one policy
             HttpStatusCode[] httpStatusCodesWorthRetrying = {
                HttpStatusCode.RequestTimeout, // 408
                HttpStatusCode.InternalServerError, // 500
@@ -44,67 +38,12 @@ namespace GeoDataService.Controllers
                HttpStatusCode.GatewayTimeout // 504
             };
 
-
-            //these two at least work
-            //var fallbackPolicy = Policy.Handle<Exception>()
-            //    .FallbackAsync(async (cancellationToken) => await GetFromDatabase(streetAddress));
-
-            //var retryPolicy = Policy
-            ////.Handle<Exception>()
-            //.HandleResult<Task<MapServerReturnResult>>(r => httpStatusCodesWorthRetrying.Contains(r.Result.HttpResponseStatusCode))
-            //.OrResult(r => r.Result == null)
-            //.RetryAsync(1);
-
-
-
-            //var result = await fallbackPolicy.ExecuteAsync(async () => await GetResultFromLocatorApi(x, y, streetAddress, mapServerClient))
-            //var result = await fallbackPolicy.ExecuteAsync(async () => await GetResultFromLocatorApi(x, y, streetAddress, mapServerClient)).
-
-            //var result = fallbackPolicy.WrapAsync(retryPolicy).ExecuteAsync(async () => await GetResultFromLocatorApi(x, y, streetAddress, mapServerClient);
-
-            //var result = await fallbackPolicy.WrapAsync(retryPolicy)
-            //    //.ExecuteAsync(async () => await GetResultFromLocatorApi(x, y, streetAddress, mapServerClient)).ConfigureAwait(false)
-            //    .ExecuteAsync(() =>  GetResultFromLocatorApi(x, y, streetAddress, mapServerClient) );
-
-
-
-            //var policyWrap = Policy.Wrap(retryPolicy, fallbackPolicy);
-
-
-
-            //var result = await Policy
-            //    .Handle<Exception>()
-            //    .OrResult<Task<MapServerReturnResult>>(r => httpStatusCodesWorthRetrying.Contains(r.Result.HttpResponseStatusCode))
-            //    .Fallback<Task<MapServerReturnResult>>(() => GetFromDatabase(streetAddress))
-            //    .Execute(() => GetResultFromLocatorApi(x, y, streetAddress, mapServerClient));
-
-
-            //var fallBackPolicy = await Policy<Task<MapServerReturnResult>>
-            //    .Handle<Exception>()
-            //    //.OrResult<Task<MapServerReturnResult>>(r => httpStatusCodesWorthRetrying.Contains(r.Result.HttpResponseStatusCode))
-            //    .FallbackAsync<Task<MapServerReturnResult>>(ct =>
-            //    {
-            //        return GetFromDatabase(streetAddress).Result;
-            //        //var result = await GetFromDatabase(streetAddress);
-            //    });
-
-            //var errorCodePolicy = Policy.Handle<Task<MapServerReturnResult>>(r => httpStatusCodesWorthRetrying.Contains(r.Result.HttpResponseStatusCode))
-
-            //.FallbackAsync<Task<MapServerReturnResult>>(f => { GetFromDatabase(streetAddress) } )
-            //.Execute(() => GetResultFromLocatorApi(x, y, streetAddress, mapServerClient));
-
-            // var result = await GetResultFromLocatorApi(x, y, streetAddress, mapServerClient);
-            //var result = await fallBackPolicy.ExecuteAsync(() => GetResultFromLocatorApi(x, y, streetAddress, mapServerClient));
-
             MapServerReturnResult result;
 
-            //try
-           // {
-                var retryPolicy = Policy
-                //.Handle<Exception>()
-                  .HandleResult<MapServerReturnResult>(r => httpStatusCodesWorthRetrying.Contains(r.HttpResponseStatusCode))
-                  .OrResult(r => r == null)
-                  .RetryAsync(1);
+            var retryPolicy = Policy
+                .HandleResult<MapServerReturnResult>(r => httpStatusCodesWorthRetrying.Contains(r.HttpResponseStatusCode))
+                .OrResult(r => r == null)
+                .RetryAsync(1);
 
             var apiResult = await retryPolicy.ExecuteAsync(async () => await GetResultFromLocatorApi(x, y, streetAddress, mapServerClient));
 
@@ -124,13 +63,6 @@ namespace GeoDataService.Controllers
                 //Elmah will raise an exception
                 throw new HttpResponseException(responseMessage);
             }
-
-            //if (result.HttpResponseStatusCode != HttpStatusCode.OK)
-            //{
-            //    var responseMessage = Request.CreateResponse(result.HttpResponseStatusCode);
-            //    responseMessage.Content = new StringContent(result.ErrorMessage);
-            //    throw new HttpResponseException(responseMessage);
-            //}
 
             return Ok(result);
         }
